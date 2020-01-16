625dfa4bdcfa02312ce6eafc6c4842101f4c2021
joeunseong <euns29@naver.com>
2020-01-16 18:01:50 +0900

    non-static nested class 적용

--- src/main/java/jes/movie/util/AbstractList.java
+++ src/main/java/jes/movie/util/AbstractList.java
@@ -9,16 +9,17 @@ public abstract class AbstractList<E> implements List<E> {
 
   @Override
   public Iterator<E> iterator() {
-    return new ListIterator<E>(this);
+    return this.new ListIterator<E>();
   }
   
 
- static class ListIterator<E> implements Iterator<E> {
+ class ListIterator<E> implements Iterator<E> {
   List<E> list;
   int cursor;
   
-  public ListIterator(List<E> lis) {
-    this.list = list;
+  @SuppressWarnings("unchecked")
+  public ListIterator() {
+    this.list = (List<E>)AbstractList.this;
   }
 
   @Override
