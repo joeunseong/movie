3464852d321f415c22b53485977b112d7f309413
joeunseong <euns29@naver.com>
2020-02-11 18:56:42 +0900

    server 생성

---
+++ movie-server/src/main/java/jes/movie/ServerApp.java
@@ -0,0 +1,11 @@
+package jes.movie;
+
+public class ServerApp {
+  public String getGreeting() {
+    return "Hello world.";
+  }
+
+  public static void main(String[] args) {
+    System.out.println("서버 수업관리 시스템");
+  }
+}
---
+++ movie-server/src/test/java/jes/movie/ServerAppTest.java
@@ -0,0 +1,12 @@
+
+package jes.movie;
+
+import org.junit.Test;
+
+public class ServerAppTest {
+  @Test
+  public void testAppHasAGreeting() {
+    // ServerApp classUnderTest = new ServerApp();
+    // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+}
