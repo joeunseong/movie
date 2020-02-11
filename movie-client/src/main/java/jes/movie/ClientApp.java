fe822aee4b2ca7ceb62482b7d79019b1df281a62
joeunseong <euns29@naver.com>
2020-02-11 19:22:46 +0900

    서버와 클라이언트를 이용해 간단한 메시지 주고 받기

--- movie-client/src/main/java/jes/movie/ClientApp.java
+++ movie-client/src/main/java/jes/movie/ClientApp.java
@@ -1,8 +1,56 @@
 package jes.movie;
 
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientApp {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     System.out.println("클라이언트 수업 관리 시스템입니다");
+    String serverAddr = null;
+    int port = 0;
+
+    // 키보드 스캐너 준비
+    Scanner keyScan = new Scanner(System.in);
+
+    try {
+      System.out.print("서버? ");
+      serverAddr = keyScan.nextLine();
+
+      System.out.print("포토? ");
+      port = Integer.parseInt(keyScan.nextLine());
+    } catch (Exception e) {
+      System.out.println("서버 주소 또는 포트 번호가 유효하지 않습니다.");
+      keyScan.close();
+      return;
+    }
+    try (
+        // 서버와 연결
+        Socket socket = new Socket(serverAddr, port);
+
+        // 소켓을 ㅌ오해 데이터를 읽고 쓰는 도구를 준비한다.
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        Scanner in = new Scanner(socket.getInputStream())) {
+      System.out.println("서버와 연결되었음!");
+      String sendMsg = keyScan.nextLine();
+
+      // 서버에 메시지 전송
+      out.println(sendMsg);
+      System.out.println("서버에 메시지를 전송하였음!");
+
+      // 서버가 응답한 메시지를 수신한다.
+      String message = in.nextLine();
+      System.out.println("서버로부터 메시지를 수신하였음!");
+
+      // 서버가 받은 메시지를 출력
+      System.out.println("서버 :" + message);
+
+      System.out.println("서버와 연결을 끊었음!");
+    } catch (Exception e) {
+      System.out.println("예외발생:");
+      e.printStackTrace();
+    }
+    keyScan.close();
   }
 }
--- movie-server/src/main/java/jes/movie/ServerApp.java
+++ movie-server/src/main/java/jes/movie/ServerApp.java
@@ -1,11 +1,56 @@
 package jes.movie;
 
-public class ServerApp {
-  public String getGreeting() {
-    return "Hello world.";
-  }
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class ServerApp {
   public static void main(String[] args) {
     System.out.println("서버 수업관리 시스템");
+    try (
+        // 서버에 연결 준비
+        ServerSocket serverSocket = new ServerSocket(9999)) {
+      System.out.println("클라이언트 연결 대기중...");
+
+      while (true) {
+        // 서버에 대기하고 있는 클라이언트와 연결
+        Socket socket = serverSocket.accept();
+        System.out.println("클라이언트와 연결되었음!");
+
+        // 클라이언트 요청 처리
+        processRequest(socket);
+        System.out.println("----------------------");
+      }
+    } catch (Exception e) {
+      System.out.println("서버 준비 중 오류 발생");
+      return;
+    }
+  }
+
+  static void processRequest(Socket clientSocket) {
+    try (
+        // 요청 처리가 끝난 후 클라이언트와의 연결 자동으로 끊기
+        Socket socket = clientSocket;
+
+        // 클라이언트의 메시지를 수신하고 전송할 입출력 도구 준비
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintStream out = new PrintStream(socket.getOutputStream())) {
+      System.out.println("통신을 위한 입출력 스트림을 준비하였음!");
+
+      // 클라이언트가 보낸 메시지를 수신
+      String message = in.nextLine();
+      System.out.println("클라이언트가 보낸 메시지를 수신하였음!");
+      System.out.println("클라이언트" + message);
+
+      // 클라이언트에게 메시지를 전송
+      out.print("Hi(조은성)");
+      System.out.println("클라이언트로 메시지를 전송하였음!");
+
+    } catch (Exception e) {
+      System.out.println("예외발생:");
+      e.printStackTrace();
+    }
+
   }
 }
