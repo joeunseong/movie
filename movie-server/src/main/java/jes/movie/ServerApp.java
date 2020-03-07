5aee83c2066cbfba092276ddbeaa37a84f83e533
joeunseong <euns29@naver.com>
2020-03-07 23:58:58 +0900

    Stateful을 Stateless 통신 방식으로 바꾸기

--- movie-server/src/main/java/jes/movie/ServerApp.java
+++ movie-server/src/main/java/jes/movie/ServerApp.java
@@ -107,23 +107,19 @@ public class ServerApp {
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
       System.out.println("통신을 위한 입출력 스트림을 준비하였음!");
 
-      while (true) {
         String request = in.readUTF();
         System.out.println("클라이언트가 보낸 메시지를 수신하였음!");
 
-        switch (request) {
-          case "quit":
-            quit(out);
-            return 0;
-          case "/server/stop":
-            quit(out);
-            return 9;
+        if(request.equalsIgnoreCase("/server/stop")) {
+          quit(out);
+          return 9;
         }
 
         Servlet servlet = servletMap.get(request);
         if (servlet != null) {
           try {
             servlet.service(in, out);
+            
           } catch (Exception e) {
             out.writeUTF("FAIL");
             out.writeUTF(e.getMessage());
@@ -134,10 +130,11 @@ public class ServerApp {
         } else {
           notFound(out);
         }
+        
         out.flush();
         System.out.println("클라이언트에게 응답하였음!");
-        System.out.println("----------------------------");
-      }
+        return 0;
+
     } catch (Exception e) {
       System.out.println("예외 발생:");
       e.printStackTrace();
